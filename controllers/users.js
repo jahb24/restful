39c0fc9d7d3086d9e2e84cb414692c9b887b959a
aaron <a329747@uach.mx>
2021-09-21 16:15:38 -0600

    se hizo modificación en rest

--- controllers/users.js
+++ controllers/users.js
@@ -2,40 +2,27 @@ const express = require('express')
 
 //restfull => get, post, put, patch, delete
 //modelo = estructura de datos que representa una entidad de mundo real
+
 function list(req, res, next) {
-    const n1 = req.body.n1;
-    const n2 = req.body.n2;
-    const suma = n1 + n2;
-    res.send(`la suma de ${n1} y ${n2} es ${suma}`);
+    res.send('Lista de usuarios del sistema');
 }
 function index(req, res, next){
-    res.send(`usuario del sistema con un id = ${req.params.id}`)
+    res.send(`usuario del sistema con un id =  ${req.params.id}`)
 }
 function create(req, res, next){
-    const n1 = req.body.n1;
-    const n2 = req.body.n2;
-    const multi = n1 * n2;
-    res.send(`la multiplicacion de ${n1} y ${n2} es ${multi}`)
+    const name = req.body.name;
+    const lastName = req.body.lastName;
+    res.send(`crear un usuario nuevo con nombre ${name} y apellido ${lastName}`)
 }
 function replace(req, res, next){
-    const n1 = req.body.n1;
-    const n2 = req.body.n2;
-    const division = n1 / n2;
-    res. send(`la division de ${n1} y ${n2} es ${division}`)
+    res. send(`reemplazo un usuario con id = ${req.params.id} por otro`)
 }
 function edit(req, res, next){
-    const n1 = req.body.n1;
-    const n2 = req.body.n2;
-    const potencia = Math.pow(n1, n2);
-    res. send(`${n1} elevado a ${n2} es ${potencia}`)
+    res. send(`reemplazo las propiedades de un usuario con id = ${req.params.id} por otras`)
 }
 function destroy(req, res, next){
-    const n1 = req.body.n1;
-    const n2 = req.body.n2;
-    const resta = n1 - n2;
-    res. send(`la resta de ${n1} y ${n2} es ${resta}`)
+    res. send(`elimino un usuario con id = ${req.params.id}`)
 }
-
 
 module.exports = {
     list, index, create, replace, edit, destroy
